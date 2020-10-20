b231a52bdf15dce4eac3fceb2139914f1f8e3311
PTLC_ <ptlc.ptlc.ptlc@gmail.com>
2020-10-20 02:19:43 +0200

    gestion des events, capture des touches clavier, ajout d'un personnage controlable

--- src/main.c
+++ src/main.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include <math.h>
+
+#define speed 6
+
+typedef struct Player {
+	float x;
+	float y;
+} Player;
 
 int main(int argc, char* argv[]) {
 	
@@ -11,12 +19,23 @@ int main(int argc, char* argv[]) {
 	}
 	
 	SDL_Window* pWindow = NULL; // Création de la fenêtre
-	pWindow = SDL_CreateWindow("A-meme-g Us - Ambi x BeXws x Rojo x Asphex", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 400, SDL_WINDOW_SHOWN);
+	pWindow = SDL_CreateWindow("A-meme-g Us - Ambi x BeXws x Rojo x Asphex", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN);
 	
 	SDL_Renderer* pRenderer = SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED); // Création du renderer (pour afficher)
 	
 	//TTF_Init(); // Démarrage de SDL_ttf pour les textes
 	//TTF_Font* font = TTF_OpenFont("assets/elected-office.regular.ttf", 256); // Chargement de la police
+	
+	// importation de la texture proto-chungus
+	SDL_Surface* image = SDL_LoadBMP("assets/proto-chungus.bmp");
+	SDL_Texture* playerTexture = SDL_CreateTextureFromSurface(pRenderer, image);
+	SDL_FreeSurface(image);
+	
+	// création d'un player
+	Player player = {0,0};
+	
+	// liste des touches pressées
+	int keysHeld[323] = {0};
 	
 	while (1) {
 		SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
@@ -28,7 +47,28 @@ int main(int argc, char* argv[]) {
 		while (SDL_PollEvent(&event)) { // les events
 			if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE)
 				endProgram = 1;
+			
+			// récupération des entrées clavier
+			if (event.type == SDL_KEYDOWN) keysHeld[event.key.keysym.scancode] = 1;
+			if (event.type == SDL_KEYUP) keysHeld[event.key.keysym.scancode] = 0;
 		}
+		
+		// actualisation du player
+		float dx = 0;
+		float dy = 0;
+		if (keysHeld[79] || keysHeld[7]) dx++; // droite
+		if (keysHeld[80] || keysHeld[4]) dx--; // gauche
+		if (keysHeld[81] || keysHeld[22]) dy++; // bas
+		if (keysHeld[82] || keysHeld[26]) dy--; // haut
+		float dl = sqrtf(dx*dx+dy*dy);
+		if (dl != 0) dx = dx / dl * speed;
+		if (dl != 0) dy = dy / dl * speed;
+		player.x += dx;
+		player.y += dy;
+		
+		// affichage du player
+		SDL_Rect r = {400-48+player.x, 400-48+player.y, 96, 96};
+		SDL_RenderCopy(pRenderer, playerTexture, NULL, &r);
 		
 		if (endProgram) break; // Condition d'arrêt du programme
 		
